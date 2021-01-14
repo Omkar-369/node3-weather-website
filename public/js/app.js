9c5520445dfb24dc09f05b2d5d2647628665f006
Omkar Shinde <oshinde78621@gmai.com>
2021-01-14 11:26:17 +0530

    Steup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ messageTwo.textContent=""
  e.preventDefault()
  const location=search.value
 
- fetch('http://localhost:3000/weather?address='+location).
+ fetch('/weather?address='+location).
  then((response)=>{
  
  response.json().then((data)=>{
